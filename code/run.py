672fa0dd655dff4e5e3afd15fe84e8a30eff5f26
yuhangz <1577748576@qq.com>
2024-05-19 17:46:04 +0800

    baseline for EMNIST classification

--- code/run.py
+++
@@ -1,7 +0,0 @@
-from torchvision import datasets, transforms
-
-transform = transforms.ToTensor()
-
-# choose the training and test datasets
-train_data = datasets.EMNIST(root='data',split='balanced', train=True,
-                                   download=False, transform=transform)
